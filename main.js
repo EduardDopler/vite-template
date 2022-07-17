af93ac37284b7b3b546c06c022a41a59bddc7776
Eduard Dopler <kontakt@eduard-dopler.de>
2022-07-17 12:28:10 +0200

    Update styles

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import "./style.css";
 import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
-  <h1 class="app__headline">Grid Walk</h1>
   <button id="counter" type="button"></button>
 `;
 
